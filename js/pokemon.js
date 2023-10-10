b544e3154b10d4a28d7c6cf6a49116699259ea97
Agustin <76200509+AgusZ02@users.noreply.github.com>
2023-10-10 11:17:16 +0200

    Update pokemon.js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,45 +1,85 @@
 class Pokemon {
+  static keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false,
+  };
+  static activePokemon = null;
 
-    // (1)
-    // (2)
+  constructor(name, sprite) {
+    this.name = name;
+    this.sprite = sprite;
+    this.element = this.createElement();
+    this.addEventListeners();
+  }
 
-    constructor(name, sprite) {
-        this.name = name;
-        this.sprite = sprite;
-        this.element = this.createElement();
-        this.addEventListeners();
-    }
+  createElement() {
+    const img = document.createElement("img");
+    img.src = this.sprite;
+    img.style.position = "absolute";
+    img.style.top = (Math.random() * 200) + "px";
+    img.style.left = (Math.random() * 200) + "px";
+    document.body.appendChild(img);
+    return img;
+  }
+
+  addEventListeners() {
+    this.element.addEventListener('click', () => {
+      Pokemon.activePokemon = this;
+      console.log("activo: " + Pokemon.activePokemon.name);
+  });
+  }
+
+  move(step) {
+    let top = parseInt(this.element.style.top);
+    let left = parseInt(this.element.style.left);
     
-    createElement() {
-      // (3) 
-    }
-    
-    addEventListeners() {   
-     	// (4)
-    }
-    
-    move(step) { 
-    	// (5)
-    }
+    if (Pokemon.keys.ArrowUp) this.element.style.top = top-step+"px";
+    if (Pokemon.keys.ArrowDown) this.element.style.top = top+step+"px";
+    if (Pokemon.keys.ArrowLeft) this.element.style.left = left-step+"px";
+    if (Pokemon.keys.ArrowRight) this.element.style.left = left+step+"px";
+  }
 } // end of Pokemon class
 
 
 document.addEventListener('keydown', function (event) {
-  
-   // (6)
+  this.keys[0] = false;
+  this.keys[1] = true;
+  this.keys[2] = false;
+  this.keys[3] = false;
+});
 
+
+document.addEventListener('keyleft', function (event) {
+  this.keys[0] = false;
+  this.keys[1] = false;
+  this.keys[2] = true;
+  this.keys[3] = false;
+});
+
+document.addEventListener('keyright', function (event) {
+  this.keys[0] = false;
+  this.keys[1] = false;
+  this.keys[2] = false;
+  this.keys[3] = true;
 });
 
 document.addEventListener('keyup', function (event) {
-   
-  // (7)
+  console.log(event);
+  this.keys[0] = true;
+  this.keys[1] = false;
+  this.keys[2] = false;
+  this.keys[3] = false;
+
 
 });
 
 function moveActivePokemon() {
-   
-  // (8) 
-
+  const step = 5;
+  if (Pokemon.activePokemon) {
+      Pokemon.activePokemon.move(step);
+  }
 }
 
 setInterval(moveActivePokemon, 10);
@@ -47,6 +87,8 @@ setInterval(moveActivePokemon, 10);
 // Instantiate Pokémon
 const pokemonNames = ['pikachu', 'bulbasaur', 'charmander', 'squirtle', 'jigglypuff'];
 
+
 pokemonNames.forEach(name => {
-    //Solicita al servidor externo la imagen del pokemon correspondiente y genera el pokemon
+  fetch("https://pokeapi.co/api/v2/pokemon/"+name).then(r=>r.json()).then(pokemon => new Pokemon(name, pokemon.sprites.front_default))
+  //Solicita al servidor externo la imagen del pokemon correspondiente y genera el pokemon
 });
